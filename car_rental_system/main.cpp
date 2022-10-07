e80264934025b141ae44c40231fe73269073cff1
godstime01 <youngestkid01@gmail.com>
2022-10-07 14:00:44 +0100

    ,ade changes to the main.cpp file

--- car_rental_system/main.cpp
+++ car_rental_system/main.cpp
@@ -9,9 +9,9 @@ class User{
   public:
   User(string u){
     username = u;
-    password = NULL;
+    //password = NULL;
   };
-  string setUsername(string n)
+  void setUsername(string n)
   {
     username = n;
   };
@@ -22,17 +22,18 @@ class User{
   {
     return password;
   };
-  //string setPassword();
+  void setPassword(string p){
+    password = p;
+  };
 };
-
 
 class AdminUser : public User
 {
   public:
   AdminUser(string n):User(n)
   {
-    username = n;
-    password = "12345";
+    //username = n;
+    setPassword("12345");
   };
   void see_reservations()
   {
@@ -50,7 +51,7 @@ class AdminUser : public User
   void displayMenu()
   {
     int op;
-    cout << "Welcome, " << username << endl;
+    cout << "Welcome, " << getUsername() << endl;
     cout << " Enter appropriate option to perfom operations " << endl;
     cout << " 1 = add cars \n " << " 2 = remove car \n " << " 3 = exit \n " << " 4 = display your reservations \n enter your option >> ";
     do{
@@ -65,16 +66,14 @@ class AdminUser : public User
       }
     }while(op != 3);
   };
-  
 };
-
 
 class Customer : public User
 {
   public:
   Customer(string n):User(n)
   {
-    username = n;
+    //username = n;
   }
   void make_payment()
   {
@@ -101,7 +100,7 @@ class Customer : public User
   void displayMenu()
   {
     int op;
-    cout << "Welcome, " << username << endl;
+    cout << "Welcome, " << getUsername() << endl;
     cout << " Enter appropriate option to perfom operations " << endl;
     cout << " 1 = make reservations \n " << " 2 = cancel reservations \n " << " 3 = exit \n " << " 4 = display your reservations \n enter your option >> ";
     do{
@@ -114,9 +113,9 @@ class Customer : public User
         case 4:
           see_reservations(); break;
       }
-    }while(op != 3)
-  };
-}
+    }while(op != 3);
+};
+};
 
 class Car
 {
@@ -142,19 +141,18 @@ class Car
     {
       state = s;
     };
-    floating getState()
+    string getState()
     {
       return state;
     };
   private:
     string name, state;
     float price;
-}
-
-
+};
 
 void welcome()
-{ int op; string n;
+{ 
+  int op; string n, p;
   cout << " ======= CAR RENTAL SYSTEM ====== \n\n" <<endl;
   do 
   {
@@ -163,27 +161,25 @@ void welcome()
     switch(op)
     {
       case 1:
-      cout << "Enter your name >> ";
-      cin >> n;
-        Customer c(n);
-        c.displayMenu(); // will display menunwith functionality of Customer
+        cout << "Enter your name >> ";
+        cin >> n;
+        //Customer c(n);
+       // c.displayMenu(); // will display menu nwith functionality of Customer
         break;
-       case 2:
-       cout << " You must login you view admin panel " << endl;
-       cout << "Enter your username >> ";
-       cin >> n;
-       cout << "Enter your password >> ";
-       cin >> p;
-       AdminUser a(n);
-       if(a.getPassword() == p){
-         a.displayMenu();
-       }else{
-         cout << " wrong password " << endl;
-       }
-       break;
-    }
-  }while(op != 00)
-}
+      case 2:
+         cout << " You must login you view admin panel " << endl;
+         cout << "Enter your username >> ";
+         cin >> n;
+         cout << "Enter your password >> ";
+         cin >> p;
+         AdminUser a(n);
+         if(a.getPassword() == p){
+           a.displayMenu();
+          };
+        break;
+    };
+  }while(op != 00);
+};
 
 int main()
 {
